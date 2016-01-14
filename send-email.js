5bd38ddebe7c6547c3054240b4e9dcddd3d9cd85
acarribasplatag <anac.arribasplata@gmail.com>
2016-01-14 18:12:42 -0500

    trying sending emails

---
+++ send-email.js
@@ -0,0 +1,26 @@
+function log(obj) {
+    $('#response').text(JSON.stringify(obj));
+}
+
+var m = new mandrill.Mandrill('KsnxCiHbpEqZVP3-leqIxQ');
+
+
+function sendTheMail() {
+// Send the email!
+var mailaddress = document.getElementById("maila").value,
+recipient = [{"email": String(mailaddress)}];
+var params = {
+    "message": {
+        "from_email":"anac.arribasplata@gmail.com",
+        "to": recipient,
+        "subject": "Sending a text email from the Mandrill API",
+        "text": "I'm learning the Mandrill API at Codecademy.",
+        "autotext": true
+    }
+};
+    m.messages.send(params, function(res) {
+        log(res);
+    }, function(err) {
+        log(err);
+    });
+}
