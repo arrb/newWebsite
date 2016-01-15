f1ea85ed66c77c15950d83db6afc61ecbd9d9802
acarribasplatag <anac.arribasplata@gmail.com>
2016-01-15 16:10:12 -0500

    Fixed sending emails

--- send-email.js
+++ send-email.js
@@ -1,23 +1,40 @@
 function log(obj) {
-    $('#response').text(JSON.stringify(obj));
+    var emailEmp = document.getElementById("emailEmp");
+    var messageEmp = document.getElementById("messageEmp");
+	var namEmpl = document.getElementById("nameEmpl").value;
+    var nameToClear = document.getElementById("nameEmpl");
+	
+    emailEmp.value = "";
+    messageEmp.value = "";
+	
+    $('#formGroup').hide();
+    $("#response").text("Thank You " + namEmpl + " I will get back to you shortly.").show().delay(5000).queue(function(n) {
+        nameToClear.value = "";
+        $(this).hide();
+        n();
+        $('#formGroup').show();
+    });
 }
 
 var m = new mandrill.Mandrill('KsnxCiHbpEqZVP3-leqIxQ');
 
-
 function sendTheMail() {
-// Send the email!
-var mailaddress = document.getElementById("maila").value,
-recipient = [{"email": String(mailaddress)}];
-var params = {
-    "message": {
-        "from_email":"ana.arribasplata@aarribas.com",
-        "to": recipient,
-        "subject": "Sending a text email from the Mandrill API",
-        "text": "I'm learning the Mandrill API at Codecademy.",
-        "autotext": true
-    }
-};
+   
+    var namEmpl = document.getElementById("nameEmpl").value;
+    var emailEmp = document.getElementById("emailEmp").value;
+    var messageEmp = document.getElementById("messageEmp").value;
+
+    var params = {
+        "message": {
+            "from_email": "ana.arribasplata@aarribas.com",
+            "to": [{
+                "email": "ana.arribasplata@aarribas.com"
+            }],
+            "subject": emailEmp,
+            "text": namEmpl + " " + messageEmp,
+            "autotext": true
+        }
+    };
     m.messages.send(params, function(res) {
         log(res);
     }, function(err) {
